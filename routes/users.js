64d7c2c8469f688284c0c17059b2d38810c40d4e
RVerma90 <irahulverma90@gmail.com>
2018-05-16 19:27:51 +0100

    backend with react frontend

--- routes/users.js
+++ routes/users.js
@@ -7,6 +7,7 @@ router.get('/', function(req, res, next) {
     {id: 1, username: "user1"},
     {id: 2, username: "user2"},
     {id: 3, username: "user3"},
+    {id: 4, username: "user4"},
   ]);
 });
 
