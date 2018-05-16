e0201dd39500e05db144b595a0df0b9abea515b7
RVerma90 <irahulverma90@gmail.com>
2018-05-16 19:08:44 +0100

    dummy data

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.json([
+    {id: 1, username: "user1"},
+    {id: 2, username: "user2"},
+    {id: 3, username: "user3"},
+  ]);
 });
 
 module.exports = router;
